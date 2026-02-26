ae2dd25780341566ddde817aec796bcd15f77be3
Idris Kalouache <idris@domatix.com>
2026-02-26 08:16:03 +0000

    Añadido Vista, lista y formulario

--- music_school_idris/__manifest__.py
+++ music_school_idris/__manifest__.py
@@ -11,6 +11,7 @@
         'base'
     ],
     'data':[
+        'security/res_groups.xml',
         'security/ir.model.access.csv',
         'views/music_school_student_views.xml',
         'views/music_school_instrument_views.xml',
--- music_school_idris/models/music_school_student.py
+++ music_school_idris/models/music_school_student.py
@@ -10,3 +10,13 @@ class MusicSchoolStudent(models.Model):
     phone = fields.Char(string="Phone")
     birthdate = fields.Date(string="BirthDate")
     age = fields.Integer(string="Age")
+    user_id = fields.Many2one(
+        comodel_name='res.users', 
+        string="Responsible",
+        help="The user responsible for this student"
+        )
+    active = fields.Boolean(default=True)
+    notes = fields.Html(
+        string="Notes",
+        help ="Additional information about the student, such as preferences or special needs."
+        )
